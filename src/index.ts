912149ca41a73d3fd52cb79d4fe2803d80b8c3e5
Alex Tiley <alex@tiley.io>
2019-08-02 11:21:58 +0100

    chore: reexport puppeteer typescript interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import pdf from './middleware/pdf';
 import { ExpressPDFOptions } from './types';
+import { LaunchOptions } from 'puppeteer';
 
+// TODO Revisit these exports
 declare global {
   namespace Express {
     export interface Response {
@@ -8,5 +10,7 @@ declare global {
     }
   }
 }
+
+export { ChromeArgOptions, LaunchOptions, PDFOptions } from 'puppeteer';
 
 export default pdf;
