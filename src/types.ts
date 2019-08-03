e4f0e77ef354e0d5b8861aa6315331fb55f0aad3
Alex Tiley <alex@tiley.io>
2019-08-03 22:00:08 +0100

    chore: attempt export of interfaces

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,5 @@
 import { LaunchOptions, PDFOptions } from 'puppeteer';
 
-export {
-  BrowserOptions,
-  ChromeArgOptions,
-  LayoutDimension,
-  PDFFormat,
-  PDFOptions
-} from 'puppeteer';
-
 export interface HTMLToPDFOptions {
   disposition?: 'inline' | 'attachment';
   filename?: string;
