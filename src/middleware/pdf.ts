8333d7a08c20e799cbea3b66e5d7763c024c1f31
Alex Tiley <alex@tiley.io>
2019-08-04 09:37:49 +0100

    chore: expose puppeteer configuration types

--- src/middleware/pdf.ts
+++ src/middleware/pdf.ts
@@ -1,15 +1,5 @@
 import { handlePDF } from '../handlers/pdf';
-import { HTMLToPDFOptions } from '../types';
 import { RequestHandler } from 'express';
-
-// Augment express response object to allow support for res.pdf()
-declare global {
-  namespace Express {
-    interface Response {
-      pdf: (html: string, options?: HTMLToPDFOptions) => Promise<this>
-    }
-  }
-}
 
 const middleware: RequestHandler = (req, res, next) => {
   res.pdf = handlePDF;
